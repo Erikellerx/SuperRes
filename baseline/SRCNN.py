a1c3d71fc4fb207cafb837e92e4f23ec3e5e87aa
Erikellerx <huahua_eric_1999@vip.sina.com>
2024-11-07 14:45:37 -0500

    add VDSR

--- baseline/SRCNN.py
+++ baseline/SRCNN.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 
 
 class SRCNN(nn.Module):
-    def __init__(self, num_channels=1):
+    def __init__(self, num_channels=3):
         super(SRCNN, self).__init__()
         self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=9, padding=9 // 2)
         self.conv2 = nn.Conv2d(64, 32, kernel_size=5, padding=5 // 2)
